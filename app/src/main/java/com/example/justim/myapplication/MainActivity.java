20f10bc690186f2b601bbc85c986575e9e8dce9a
justimchung <justim@gmail.com>
2017-12-19 21:35:46 +0800

    在主畫面中，如果切到我的最愛的頁面時， FavoriateComicItemFragment 要 refresh, 展示最新的 favoriate comic. 刪除 MyFavoriateComicItemRecyclerViewAdapter 因為不需要它

--- app/src/main/java/com/example/justim/myapplication/MainActivity.java
+++ app/src/main/java/com/example/justim/myapplication/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,8 +14,34 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ViewPager vpFragment = (ViewPager) findViewById(R.id.vpFragment);
-        vpFragment.setAdapter(new ComicHouseFragmentPageAdapter(getSupportFragmentManager()));
+        final ViewPager vpFragment = (ViewPager) findViewById(R.id.vpFragment);
+        final ComicHouseFragmentPageAdapter adapter = new ComicHouseFragmentPageAdapter(getSupportFragmentManager());
+        vpFragment.setAdapter(adapter);
+        vpFragment.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                if(position == 1) {
+                    int index = vpFragment.getCurrentItem();
+                    FavoriateComicItemFragment tabFragment = (FavoriateComicItemFragment) adapter.instantiateItem(vpFragment, index);
+                    tabFragment.reflash();
+                    RecyclerView rclist = (RecyclerView) tabFragment.getView().findViewById(R.id.list);
+                    rclist.getAdapter().notifyDataSetChanged();
+                }
+
+
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
 
         TabLayout tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
         tabLayout.setupWithViewPager(vpFragment);
