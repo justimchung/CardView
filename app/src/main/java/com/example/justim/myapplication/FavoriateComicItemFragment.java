ca822715520286e2566461eb99cfba7f246750b6
justim <justim@gmail.com>
2017-12-20 10:26:02 +0800

    移除不需要的 import 檔

--- app/src/main/java/com/example/justim/myapplication/FavoriateComicItemFragment.java
+++ app/src/main/java/com/example/justim/myapplication/FavoriateComicItemFragment.java
@@ -1,18 +1,12 @@
 package com.example.justim.myapplication;
 
-import android.content.Context;
+
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 
 import com.example.justim.myapplication.comicitem.ComicContent;
-import com.example.justim.myapplication.dummy.DummyContent;
-import com.example.justim.myapplication.dummy.DummyContent.DummyItem;
+
 
 import java.util.ArrayList;
 import java.util.List;
